2b4f35e2697166c36118dd9934678dc9fe10888d
jason <bjason@gmail.com>
2017-05-20 11:32:55 -0700

    Staticindex

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const path = require('path');
 
 app.use("/api", routes);
 
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile( path.join(__dirname, 'public', 'index.html'));
+});
 // Setup static routing:
 app.use("/static", express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-  res.send("Hello world3!");
-});
 
 app.listen(3030, () => {
   console.log("We are starting the app on port 3030");
