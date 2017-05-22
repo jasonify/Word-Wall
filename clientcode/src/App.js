8d1096a028562e1f24973f14bbbf18dd288e817a
jason <bjason@gmail.com>
2017-05-21 18:38:50 -0700

    React renderin els

--- clientcode/src/App.js
+++ clientcode/src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      words: []
+    };
+  }
   componentDidMount() {
 
     axios.post('http://localhost:3030/api/word', {
@@ -20,21 +26,30 @@ class App extends Component {
     axios.get('http://localhost:3030/api/words')
     .then(res => {
       console.log('res', res);
+      const words = res.data.results;
       // const posts = res.data.data.children.map(obj => obj.data);
       // console.log('posts', posts);
-      // this.setState({ posts });
+      this.setState({ words });
     });
   }
 
   render() {
+    var wordEls = [];
+    let words = this.state.words;
+    words.forEach((word)=> {
+      console.log( word);
+      wordEls.push(<div> {word}  </div>);
+    });
+    // for(var ii = 0; ii < 
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React :/</h2>
+          <h2>Word Wall /</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         
+          {wordEls}
         </p>
       </div>
     );
